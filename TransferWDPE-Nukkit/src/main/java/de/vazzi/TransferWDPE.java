ca5abf5cc80cb5549b0ba0387866e9196f5cdcd3
Pigeon2333 <68182691+Pigeon2333@users.noreply.github.com>
2021-08-10 17:13:15 +0800

    Update TransferWDPE.java

--- TransferWDPE-Nukkit/src/main/java/de/vazzi/TransferWDPE.java
+++ TransferWDPE-Nukkit/src/main/java/de/vazzi/TransferWDPE.java
@@ -1,23 +1,20 @@
-package de.vazzi;
-
+package vazzi;
 
 import cn.nukkit.Player;
 import cn.nukkit.network.protocol.TransferPacket;
 import cn.nukkit.plugin.PluginBase;
 
 public class TransferWDPE extends PluginBase {
-
-    @Override
-    public void onEnable() {
-        getServer().getCommandMap().register("TransferWDPE", new TransferCommand(this));
+    public TransferWDPE() {
     }
 
-    public static void transferPlayer(Player player, String proxyserver){
+    public void onEnable() {
+        this.getServer().getCommandMap().register("TransferWDPE", new TransferCommand(this));
+    }
+
+    public static void transferPlayer(Player player, String proxyserver) {
         TransferPacket transferPacket = new TransferPacket();
         transferPacket.address = proxyserver;
-
-        //port is default 19132
         player.dataPacket(transferPacket);
     }
-
 }
