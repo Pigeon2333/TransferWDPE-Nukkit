440378cb6cfc2db72e76691f67d193fe10f339a5
Pigeon2333 <68182691+Pigeon2333@users.noreply.github.com>
2021-08-10 17:12:30 +0800

    Update TransferCommand.java

--- TransferWDPE-Nukkit/src/main/java/de/vazzi/TransferCommand.java
+++ TransferWDPE-Nukkit/src/main/java/de/vazzi/TransferCommand.java
@@ -7,49 +7,42 @@ import cn.nukkit.command.PluginIdentifiableCommand;
 import cn.nukkit.plugin.Plugin;
 
 public class TransferCommand extends Command implements PluginIdentifiableCommand {
-
     private Plugin plugin = null;
 
     public TransferCommand(Plugin plugin) {
         super("wtransfer", "Transfer to another proxy server.");
-        setPermission("wtransfer.command");
-        setUsage("/wtransfer server | /wtransfer player server");
+        this.setPermission("wtransfer.command");
+        this.setUsage("/wtransfer server | /wtransfer player server");
         this.plugin = plugin;
     }
 
-    @Override
     public boolean execute(CommandSender commandSender, String s, String[] strings) {
-
-        if(!commandSender.hasPermission(getPermission())){
+        if (!commandSender.hasPermission(this.getPermission())) {
             return true;
-        }
-
-        if(strings.length < 1){
+        } else if (strings.length < 1) {
             return true;
-        }
-
-        if(strings.length < 2){
-            if(commandSender.isPlayer()) {
+        } else if (strings.length < 2) {
+            if (commandSender.isPlayer()) {
                 commandSender.sendMessage("§fYou will be transfered to §a" + strings[0]);
-                TransferWDPE.transferPlayer((Player) commandSender, strings[0]);
+                TransferWDPE.transferPlayer((Player)commandSender, strings[0]);
+                return true;
+            } else {
                 return true;
             }
-            return true;
+        } else {
+            Player targetplayer = this.getPlugin().getServer().getPlayer(strings[0]);
+            if (targetplayer != null) {
+                String var10001 = targetplayer.getName();
+                commandSender.sendMessage("§fPlayer §a" + var10001 + " §fwill be transfered to §a" + strings[1]);
+                TransferWDPE.transferPlayer(targetplayer, strings[1]);
+                return true;
+            } else {
+                return true;
+            }
         }
-
-        Player targetplayer = getPlugin().getServer().getPlayer(strings[0]);
-        if(targetplayer != null) {
-            commandSender.sendMessage("§fPlayer §a" + targetplayer.getName()  + " §fwill be transfered to §a" + strings[1]);
-            TransferWDPE.transferPlayer(targetplayer, strings[1]);
-            return true;
-        }
-
-        return true;
-
     }
 
-    @Override
     public Plugin getPlugin() {
-        return plugin;
+        return this.plugin;
     }
 }
